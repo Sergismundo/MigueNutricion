87f43bb8abe70ffed06b19505604cc59e7522e9d
DavidEspinola <davidep06@gmail.com>
2020-09-02 00:51:35 +0200

    Initial implementation

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pug Sass boilerplate
+
+## Usage
+Install dependencies:
+`npm install`
+
+Build project:
+`npm run build`
+
+Serve in development mode (recompilation and live-reload):
+
+```npm run dev:build```
+
+```npm run dev:serve```
+
+(Need 2 separate terminal tabs)
+
+## Recomended VSCode Extensions
+- [Sass](https://marketplace.visualstudio.com/items?itemName=Syler.sass-indented)
---
+++ build.js
@@ -0,0 +1,25 @@
+const pug = require('pug');
+const glob = require('glob');
+const fs = require('fs-extra');
+const sass = require('sass');
+
+const sassFiles = glob.sync('src/**/*.sass');
+const pageFiles = glob.sync('src/pages/**/*.pug');
+
+fs.removeSync('dist')
+
+sassFiles.forEach(path => {
+    const fileContent = sass.renderSync({file: path}).css.toString()
+    const destination = toDestPath(path, 'sass', 'css')
+    fs.outputFileSync(destination, fileContent)
+})
+
+pageFiles.forEach(path => {
+    const fileContent = pug.renderFile(path, { basedir: 'src' })
+    const destination = toDestPath(path, 'pug', 'html')
+    fs.outputFileSync(destination, fileContent)
+})
+
+function toDestPath(path, originExtension, destExtension) {
+    return path.replace('src', 'dist').replace('pages', '').replace(originExtension, destExtension)
+}
